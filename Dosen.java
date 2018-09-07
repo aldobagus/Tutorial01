dbe3b9a29c792b8550fe58183210b1a984ee1869
Aldo <aldobagus@rocketmail.com>
2018-09-07 17:03:00 +0700

    Initial Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,20 @@
+public class Dosen {
+    private int noID;
+    private String nama;
+ 
+    public int getnoID() {
+        return this.noID;
+    }
+ 
+    public void setnoID(int noID) {
+        this.noID = noID;
+    }
+
+    public int getNama() {
+        return this.nama;
+    }
+ 
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
