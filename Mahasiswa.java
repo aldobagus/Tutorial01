5b7f913d44e3dfed745f6f79a84bc421530bc0e2
Aldo <aldobagus@rocketmail.com>
2018-09-07 16:58:52 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+public class Mahasiswa {
+    private int npm;
+    private String nama;
+ 
+    public int getNpm() {
+        return this.npm;
+    }
+ 
+    public void setNpm(int npm) {
+        this.npm = npm;
+    }
+
+    public int getNama() {
+        return this.nama;
+    }
+ 
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
